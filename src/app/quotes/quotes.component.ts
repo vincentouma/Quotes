715a27f416565b81d7dbc395cf430844d557c020
vincentouma <vinceoumah@gmail.com>
2019-07-06 15:40:10 +0300

    Add toggle effect to hide quoes and show upn clicking button

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,13 +9,18 @@ import { quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quotes:quotes [] = [
-    {id:1, name:'Watch finding Nemo',description:'Find an online version and watch merlin find his son'},
-    {id:2,name:'Buy Cookies',description:'I have to buy cookies for the parrot'},
-    {id:3,name:'Get new Phone Case',description:'Diana has her birthday coming up soon'},
-    {id:4,name:'Get Dog Food',description:'Pupper likes expensive sancks'},
-    {id:5,name:'Solve math homework',description:'Damn Math'},
-    {id:6,name:'Plot my world domination plan',description:'Cause I am an evil overlord'},
-  ];
+    new quotes (1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
+    new quotes (2,'Buy Cookies','I have to buy cookies for the parrot'),
+    new quotes(3,'Get new Phone Case','Diana has her birthday coming up soon'),
+    new quotes (4,'Get Dog Food','Pupper likes expensive snacks'),
+    new quotes (5,'Solve math homework','Damn Math'),
+    new quotes (6,'Plot my world domination plan','Cause I am an evil overlord'),
+  ];toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
+
+ 
 
   constructor() { }
 
